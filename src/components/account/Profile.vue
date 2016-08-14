69980302d91f82a3bc71f334a0a326d001f54a55
Lijy91 <lijy91@foxmail.com>
2016-08-14 22:49:55 +0800

    账号和安全Tab

--- src/components/account/Profile.vue
+++ src/components/account/Profile.vue
@@ -63,7 +63,45 @@
           </div>
           <div class="tab-pane" id="account" role="tabpanel">
             <p>
-              ...
+              <div class="form-group row">
+                <label for="username-input" class="col-xs-2 col-form-label">用户名</label>
+                <div class="col-xs-10">
+                  <p class="form-control-static">{{ user.username }}</p>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="email-input" class="col-xs-2 col-form-label">邮箱</label>
+                <div class="col-xs-10">
+                  <input class="form-control" type="name" value="{{ user.email }}" id="email-input" placeholder="">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="mobile-input" class="col-xs-2 col-form-label">手机</label>
+                <div class="col-xs-10">
+                  <input class="form-control" type="text" value="{{ user.mobile }}" id="mobile-input" placeholder="">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="password-input" class="col-xs-2 col-form-label">帐号密码</label>
+                <div class="col-xs-10">
+                  <p class="form-control-static">
+                    <a href="#">修改密码</a>
+                  </p>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="bio-input" class="col-xs-2 col-form-label">社交帐号</label>
+                <div class="col-xs-10">
+                  <p class="form-control-static">
+                    <a href="#"> 绑定微信</a>
+                    <a href="#"> 绑定微信</a>
+                    <a href="#"> 绑定GitHub</a>
+                  </p>
+                </div>
+              </div>
+              <div class="form-group">
+                <button type="submit" class="btn btn-primary">保存</button>
+              </div>
             </p>
           </div>
           <div class="tab-pane" id="settings" role="tabpanel">
