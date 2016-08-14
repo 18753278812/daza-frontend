5c3ca22473360867790881af33ec69495401cc73
Lijy91 <lijy91@foxmail.com>
2016-08-14 22:35:28 +0800

    创建分享文章页面，创建主题页面

--- src/components/article/ArticleCreate.vue
+++ src/components/article/ArticleCreate.vue
@@ -1,8 +1,40 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-xs-12 col-sm-9">
-        <p>article create</p>
+      <div class="col-sm-12">
+        <h3>发表文章</h3>
+        <hr>
+        <validator name="validation">
+          <form novalidate @submit.prevent="submit()">
+            <div class="form-group">
+              <select class="form-control">
+                <option>Default select</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <input
+                class="form-control"
+                type="text"
+                name="title"
+                placeholder="标题"
+                v-model="title"
+                v-validate:email="rules.title">
+            </div>
+            <div class="form-group">
+              <textarea
+                id="content-input"
+                class="form-control"
+                rows="15"
+                name="summary"
+                placeholder="正文"
+                v-model="summary"
+                v-validate:email="rules.summary"></textarea>
+            </div>
+            <div class="form-group">
+              <button type="submit" class="btn btn-primary" :disabled="!$validation.valid">确认</button>
+            </div>
+          </form>
+        </validator>
       </div>
     </div>
   </div>
@@ -12,6 +44,10 @@
 export default {
   data() {
     return {
+      rules: {
+        title: { required: true },
+        summary: { required: true, minlength: 15 },
+      },
     };
   },
   ready() {
--- src/components/article/ArticleShare.vue
+++ src/components/article/ArticleShare.vue
@@ -1,8 +1,61 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-xs-12 col-sm-9">
-        <p>article share</p>
+      <div class="col-sm-12">
+        <h3>分享文章</h3>
+        <hr>
+        <validator name="validation">
+          <form novalidate @submit.prevent="submit()">
+            <div class="form-group row">
+              <label for="topic-input" class="col-xs-2 col-form-label">主题</label>
+              <div class="col-xs-10">
+                <select class="form-control">
+                  <option>Default select</option>
+                </select>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="title-input" class="col-xs-2 col-form-label">标题</label>
+              <div class="col-xs-10">
+                <input
+                  class="form-control"
+                  type="text"
+                  name="title"
+                  placeholder=""
+                  v-model="title"
+                  v-validate:email="rules.title">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="link-input" class="col-xs-2 col-form-label">链接</label>
+              <div class="col-xs-10">
+                <input
+                  class="form-control"
+                  type="text"
+                  name="link"
+                  placeholder=""
+                  v-model="link"
+                  v-validate:email="rules.link">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="summary-input" class="col-xs-2 col-form-label">摘要</label>
+              <div class="col-xs-10">
+                <textarea
+                  id="summary-input"
+                  class="form-control"
+                  rows="3"
+                  name="summary"
+                  placeholder=""
+                  v-model="summary"
+                  v-validate:email="rules.summary"></textarea>
+              </div>
+            </div>
+            <div class="form-group">
+              <button type="submit" class="btn btn-primary" :disabled="!$validation.valid">确认</button>
+            </div>
+          </form>
+        </validator>
       </div>
     </div>
   </div>
@@ -12,6 +65,11 @@
 export default {
   data() {
     return {
+      rules: {
+        title: { required: true },
+        link: { required: true },
+        summary: { required: true, minlength: 15 },
+      },
     };
   },
   ready() {
