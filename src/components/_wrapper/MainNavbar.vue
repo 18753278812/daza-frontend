65e063c8ae154ab322054f99fde6e72a4f73e903
Lijy91 <lijy91@foxmail.com>
2016-08-15 19:36:45 +0800

    隐藏搜索

--- src/components/_wrapper/MainNavbar.vue
+++ src/components/_wrapper/MainNavbar.vue
@@ -4,13 +4,13 @@
       <a class="navbar-brand" v-link="'/'">
         <img alt="Brand" src="../../assets/ic_logo_light.png">
       </a>
-      <form class="form-inline pull-xs-left">
+      <!-- <form class="form-inline pull-xs-left">
         <input
           class="form-control"
           type="text"
           name="keyword"
           placeholder="搜索你感兴趣的内容">
-      </form>
+      </form> -->
       <ul class="nav navbar-nav pull-xs-right" v-if="auth.check()">
         <li class="nav-item">
           <a class="nav-link" v-link="'/notifications'">通知 <span class="tag tag-pill tag-danger">0</span></a>
@@ -20,8 +20,8 @@
           <div class="dropdown-menu dropdown-menu-right">
             <a class="dropdown-item" v-link="{ name: 'user_detail', params: { id: auth.id } }">查看个人资料</a>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" v-link="#">帮助</a>
-            <a class="dropdown-item" v-link="#">键盘快捷键</a>
+            <a class="dropdown-item" href="#">帮助</a>
+            <a class="dropdown-item" href="#">键盘快捷键</a>
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" v-link="'/account/settings'">设置</a>
             <a class="dropdown-item" v-link="'/account/logout'">退出登录</a>
