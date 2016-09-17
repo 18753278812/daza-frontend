da8443a2819b3885a33904d01fd0bbd050bf0860
Lijy91 <lijy91@foxmail.com>
2016-09-17 10:33:09 +0800

    GROWINGIO 配置

--- config/prod.env.js
+++ config/prod.env.js
@@ -6,5 +6,7 @@ module.exports = {
   EMAIL_HI: '"hi@daza.io"',
   EMAIL_REPORT: '"report@daza.io"',
   EMAIL_SUPPORT: '"support@daza.io"',
-  DAOVOICE_APPID: '"c4e91260"'
+  DAOVOICE_APPID: '"c4e91260"',
+  GROWING_ID: '"bd526e12abd6f8a0"',
+  GROWING_URL_SCHEME: '"growing.a98bf736ee7af9c3"'
 }
