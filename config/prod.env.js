299654369fa7cc1b674c178befea6311c1bca606
Lijy91 <lijy91@foxmail.com>
2016-09-17 09:16:37 +0800

    修改配置

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,9 +2,9 @@ module.exports = {
   NODE_ENV: '"production"',
   DOMAIN_NAME: '"daza.io"',
   API_URL: '"http://mock-api.daza.io"',
-  WEB_URL: '"http://mock-web.daza.io"',
+  WEB_URL: '"http://daza.io"',
   EMAIL_HI: '"hi@daza.io"',
   EMAIL_REPORT: '"report@daza.io"',
   EMAIL_SUPPORT: '"support@daza.io"',
-  DAOVOICE_APPID: '"-"'
+  DAOVOICE_APPID: '"c4e91260"'
 }
