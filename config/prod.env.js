50b08811bb97ec00dae0e2d5bf308a410a0634b9
Lijy91 <lijy91@foxmail.com>
2016-10-06 03:54:57 +0800

    修改为https

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,8 @@
 module.exports = {
   NODE_ENV: '"production"',
   DOMAIN_NAME: '"daza.io"',
-  API_URL: '"http://mock-api.daza.io"',
-  WEB_URL: '"http://mock-web.daza.io"',
+  API_URL: '"https://mock-api.daza.io"',
+  WEB_URL: '"https://mock-web.daza.io"',
   EMAIL_HI: '"hi@daza.io"',
   EMAIL_REPORT: '"report@daza.io"',
   EMAIL_SUPPORT: '"support@daza.io"',
