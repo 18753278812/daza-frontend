0e96db634128442d9c055648a1948285b70b441f
Lijy91 <lijy91@foxmail.com>
2016-11-25 10:27:23 +0800

    修改为https的url

--- config/prod.env.js
+++ config/prod.env.js
@@ -6,7 +6,7 @@ module.exports = {
   EMAIL_HI: '"hi@daza.io"',
   EMAIL_REPORT: '"report@daza.io"',
   EMAIL_SUPPORT: '"support@daza.io"',
-  QINIU_DOMAIN: '"http://obryq3mj0.bkt.clouddn.com"',
+  QINIU_DOMAIN: '"https://oeolgl6y5.qnssl.com"',
   DAOVOICE_APPID: '"c4e91260"',
   GROWING_ID: '"bd526e12abd6f8a0"',
   GROWING_URL_SCHEME: '"growing.a98bf736ee7af9c3"'
