c089b76bd3afc19d8c545d2c80131f0f7c206d74
Lijy91 <lijy91@foxmail.com>
2016-12-16 23:02:47 +0800

    Nothing

--- config/prod.env.js
+++ config/prod.env.js
@@ -6,8 +6,8 @@ module.exports = {
   EMAIL_HI: '"hi@daza.io"',
   EMAIL_REPORT: '"report@daza.io"',
   EMAIL_SUPPORT: '"support@daza.io"',
-  QINIU_DOMAIN: '"https://oeolgl6y5.qnssl.com"',
-  DAOVOICE_APPID: '"c4e91260"',
-  GROWING_ID: '"bd526e12abd6f8a0"',
-  GROWING_URL_SCHEME: '"growing.a98bf736ee7af9c3"'
+  QINIU_DOMAIN: '"http://obryns2kk.bkt.clouddn.com"',
+  DAOVOICE_APPID: '"-"',
+  GROWING_ID: '"-"',
+  GROWING_URL_SCHEME: '"-"'
 }
