30b64a0d3b1830a12818a10d7958e082938213f7
Lijy91 <lijy91@foxmail.com>
2016-12-28 00:06:31 +0800

    完成加载更多功能

--- src/components/LoadMore.vue
+++ src/components/LoadMore.vue
@@ -1,15 +1,52 @@
 <template>
   <div>
-    LoadMore
+    <button class="fluid ui basic button" v-bind:class="{ loading: isLoading }" v-on:click="submit">{{ hasMore ? '点击加载更多' : '没有更加数据'}}</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'loadmore',
+  props: {
+    pagination: {
+      type: Object,
+      required: true,
+    },
+    callback: {
+      type: Function,
+      required: true,
+    },
+  },
+  computed: {
+    currentPage() {
+      return this.pagination.current_page;
+    },
+    lastPage() {
+      return this.pagination.last_page;
+    },
+    hasMore() {
+      return this.lastPage > this.currentPage;
+    },
+  },
   data() {
     return {
+      isLoading: false,
     };
+  },
+  watch: {
+    pagination: 'watchPagination',
+  },
+  methods: {
+    watchPagination() {
+      this.isLoading = false;
+    },
+    submit() {
+      if (!this.hasMore && !this.isLoading) {
+        return;
+      }
+      this.isLoading = true;
+      this.callback(this.currentPage + 1);
+    },
   },
 };
 </script>
