b7e3c8be608f7dcc5a364891e76365959807ed90
Lijy91 <lijy91@foxmail.com>
2016-12-28 00:05:17 +0800

    Update Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,28 +3,26 @@
     <div class="ui inverted vertical footer segment" style="background-color: #212121;">
       <div class="ui sections container">
         <div class="ui grid">
-          <div class="six wide column">
+          <div class="ten wide column">
             <h4 class="ui inverted header">
               <img class="ui tiny qrcode image" src="../assets/ic_logo_light.png">
             </h4>
-            <p class="description">
+            <p class="description" style="max-width: 360px;">
               「daza.io」是一款基于技能树的技术内容聚合应用，根据你的技能对内容进行筛选，让你在这个信息过载的时代里更高效地获取你所需的内容。
             </p>
-            <div class="ui inverted bulleted horizontal link list">
-              <router-link class="item" to="/articles/about">关于我们</router-link>
-              <router-link class="item" to="/topics/help">帮助中心</router-link>
-              <router-link class="item" to="/articles/sources">获取源码</router-link>
-              <router-link class="item" to="/articles/contact">联系站长</router-link>
-              <router-link class="item" to="/articles/feedback">建议反馈</router-link>
+            <div class="computer only ui grid">
+              <div class="column">
+                <div class="ui inverted bulleted horizontal link list">
+                  <router-link class="item" to="/articles/about">关于我们</router-link>
+                  <router-link class="item" to="/topics/help">帮助中心</router-link>
+                  <router-link class="item" to="/articles/sources">获取源码</router-link>
+                  <router-link class="item" to="/articles/contact">联系站长</router-link>
+                  <router-link class="item" to="/articles/feedback">建议反馈</router-link>
+                </div>
+              </div>
             </div>
           </div>
-          <div class="four wide column">
-            <p></p>
-          </div>
-          <div class="four wide column">
-            <p></p>
-          </div>
-          <div class="two wide right aligned column">
+          <div class="six wide right aligned column">
             <h4 class="ui inverted header">客户端下载</h4>
             <div class="ui inverted link list">
               <a class="item" href="http://a.app.qq.com/o/simple.jsp?pkgname=io.daza.app" target="_blank">
@@ -39,10 +37,10 @@
     <div class="ui inverted vertical footer segment">
       <div class="ui copyright container">
         <div class="ui grid">
-          <div class="left aligned eight wide column">
+          <div class="left aligned ten wide column">
             &copy; 2017 <a href="https://daza.io" target="_blank">daza.io</a> All Rights Reserved.
           </div>
-          <div class="right aligned eight wide column">
+          <div class="computer only right aligned six wide column">
             <a href="http://www.miitbeian.gov.cn" target="_blank">粤ICP备15047595号-2</a>
           </div>
         </div>
