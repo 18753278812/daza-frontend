d0bd89feaa6115b46f3353d4cbff0736b9856f67
Lijy91 <lijy91@foxmail.com>
2017-01-03 22:09:58 +0800

    路由自定义Title

--- src/router/index.js
+++ src/router/index.js
@@ -22,14 +22,19 @@ import CategoryIndexView from '../views/categories/IndexView';
 
 import TopicIndexView from '../views/topics/IndexView';
 import TopicCreateView from '../views/topics/CreateView';
+import TopicEditView from '../views/topics/EditView';
 import TopicDetailView from '../views/topics/DetailView';
 
 import ArticleCreateView from '../views/articles/CreateView';
+import ArticleShareView from '../views/articles/ShareView';
+import ArticleEditView from '../views/articles/EditView';
 import ArticleDetailView from '../views/articles/DetailView';
 
 import TagDetailView from '../views/tags/DetailView';
 
 import NotificationIndexView from '../views/notifications/IndexView';
+
+const document = global.document;
 
 Vue.use(VueRouter);
 
@@ -46,23 +51,38 @@ const router = new VueRouter({
           path: '/',
           name: 'home_index',
           component: HomeIndexView,
+          meta: {
+            title: '首页',
+          },
         },
         {
           path: 'account/settings',
           component: AccountSettingsView,
+          meta: {
+            title: '设置',
+          },
         },
         {
           name: 'user_detail',
           path: 'users/:id',
           component: UserDetailView,
+          meta: {
+            title: '用户资料',
+          },
         },
         {
           path: 'categories/:slug',
           component: CategoryIndexView,
+          meta: {
+            title: '分类详情',
+          },
         },
         {
           path: 'categories',
           redirect: '/categories/latest',
+          meta: {
+            title: '分类',
+          },
         },
         {
           path: 'home/:slug',
@@ -71,36 +91,83 @@ const router = new VueRouter({
         {
           path: 'topics',
           component: TopicIndexView,
+          meta: {
+            title: '主题广场',
+          },
         },
         {
           path: 'topics/create',
           component: TopicCreateView,
-          meta: { requiresAuth: true },
+          meta: {
+            title: '创建主题',
+            requiresAuth: true,
+          },
+        },
+        {
+          path: 'topics/:slug/edit',
+          name: 'topic_edit',
+          component: TopicEditView,
+          meta: {
+            title: '修改主题',
+            requiresAuth: true,
+          },
         },
         {
           path: 'topics/:slug',
           name: 'topic_detail',
           component: TopicDetailView,
+          meta: {
+            title: '主题详情',
+          },
         },
         {
           path: 'articles/create',
           component: ArticleCreateView,
-          meta: { requiresAuth: true },
+          meta: {
+            title: '发表文章',
+            requiresAuth: true,
+          },
+        },
+        {
+          path: 'articles/share',
+          component: ArticleShareView,
+          meta: {
+            title: '分享文章',
+            requiresAuth: true,
+          },
+        },
+        {
+          path: 'articles/:slug/edit',
+          name: 'article_edit',
+          component: ArticleEditView,
+          meta: {
+            title: '修改文章',
+            requiresAuth: true,
+          },
         },
         {
           path: 'articles/:slug',
           name: 'article_detail',
           component: ArticleDetailView,
+          meta: {
+            title: '文章详情',
+          },
         },
         {
           path: 'tags/:name',
           name: 'tag_detail',
           component: TagDetailView,
+          meta: {
+            title: '标签详情',
+          },
         },
         {
           path: 'notifications',
           component: NotificationIndexView,
-          meta: { requiresAuth: true },
+          meta: {
+            title: '消息通知',
+            requiresAuth: true,
+          },
         },
       ],
     },
@@ -111,22 +178,37 @@ const router = new VueRouter({
         {
           path: 'register',
           component: AccountRegisterView,
+          meta: {
+            title: '注册',
+          },
         },
         {
           path: 'login',
           component: AccountLoginView,
+          meta: {
+            title: '登录',
+          },
         },
         {
           path: 'logout',
           component: AccountLogoutView,
+          meta: {
+            title: '注销中...',
+          },
         },
         {
           path: 'password_forgot',
           component: AccountPasswordForgotView,
+          meta: {
+            title: '找回密码',
+          },
         },
         {
           path: 'password_reset',
           component: AccountPasswordResetView,
+          meta: {
+            title: '重置密码',
+          },
         },
       ],
     },
@@ -135,6 +217,10 @@ const router = new VueRouter({
       component: NotFoundView,
     },
   ],
+});
+
+router.afterEach((route) => {
+  document.title = `${route.meta.title} - daza.io`;
 });
 
 router.beforeEach((to, from, next) => {
