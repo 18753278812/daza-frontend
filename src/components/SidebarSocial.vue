4aa40aeae64da312213568f1f22eaa397b9739b2
Lijy91 <lijy91@foxmail.com>
2017-01-03 18:15:21 +0800

    加入首页社交账号组件

---
+++ src/components/SidebarSocial.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="social">
+    <h3 class="ui header">关注我们</h3>
+    <div>
+      <button id="wechat" class="ui circular green icon button">
+        <i class="wechat icon"></i>
+      </button>
+      <a href="https://weibo.com/lijy91" target="_blank">
+        <button class="ui circular red icon button">
+          <i class="weibo icon"></i>
+        </button>
+      </a>
+      <a href="https://github.com/lijy91" target="_blank">
+        <button class="ui circular alternate icon button">
+          <i class="github alternate icon"></i>
+        </button>
+      </a>
+  </div>
+</template>
+
+<script>
+const $ = global.jQuery;
+
+export default {
+  props: {
+  },
+  mounted() {
+    $('#wechat')
+      .popup({
+        position: 'top center',
+        target: '#wechat',
+        html: '<h2 class="ui center aligned icon header">打开微信扫一扫<img class="ui medium image" src="/static/images/qrcode_wx.jpg" /></h2>',
+      });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.social {
+  border: 1px dashed #d3d7db;
+  padding: 16px;
+  margin-top: 10px
+}
+</style>
